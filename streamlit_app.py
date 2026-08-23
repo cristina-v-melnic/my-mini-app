4d54cbaed6f77538ada1de5cd88c09f578630aac
Cristina Melnic <103945852+cristina-v-melnic@users.noreply.github.com>
2026-08-23 12:33:17 +0000

    added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+st.text("hello")
+st.text("how are you?")
+st.text("I'm actually fine!")
