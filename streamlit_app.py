f23eab417e79b684f7ed830eca735405dda4b877
Cristina Melnic <103945852+cristina-v-melnic@users.noreply.github.com>
2026-09-08 16:02:07 +0000

    icons and last sync

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import numpy as np
 
 st.title("Random list app")
 
-tab1, tab2, tab3 = st.tabs([":dog: Top dog", "Ultimate trivia", "Recommendation"])
+tab1, tab2, tab3 = st.tabs([":dog: Top dog", "🏆 Ultimate trivia", "🔮 Recommendation"])
 
 with tab1:
     st.header("Top dog")
@@ -18,7 +18,7 @@ with tab1:
         st.checkbox("User clicks on a submit button.", key="1.3")
         st.checkbox("Feedback pop-up on whether the user guessed it or not.", key="1.4")
         st.checkbox("Give your riddle a header with and a short description.", key="1.5")
-        st.info("Useful commands: st.header(), st.write(), st.selectbox(), st.select_slider(), st.button(), st.balloons() (maybe try st radio too)")
+        st.info("Useful commands: st.header(), st.write(), st.selectbox(), st.select_slider(), st.radio(), st.button(), st.success(), st.error(), st.info(), st.balloons()")
     
     st.write(" All of these icons brought new waves of movement qualities and ideas into the choreography world. I love all of them, but...")
     favourite_things = ["Shakira", "Charlie Chaplin", "Jung Kook", "Lady Gaga", "Michael Jackson"]
@@ -42,7 +42,7 @@ with tab1:
 
 with tab2:
 
-    st.header("My trivia title")
+    st.header("Know your idols")
     with st.expander("**Exercise 2** Ultimate trivia"):
         st.write("Please create:")
         st.checkbox("A view with multiple tabs and place your first app there.", key="2.1")
@@ -51,12 +51,12 @@ with tab2:
         st.checkbox("Create a 2 question about the 1st and 2nd property, where the contents of the question are randomly appearing. i.e. Who is the `author`(column name) of the book (random item from the `books` column).", key="2.4")
         st.checkbox("Split the screen in two for each question with `st.columns()`",  key="2.5")
         st.checkbox("Give your riddle a header with and a short description.", key="2.6")
-        st.info("Useful commands: pd.DataFrame(), st.columns(), with col1:, np.random.randint(), np.sort(), st.multiselect(), ")
+        st.info("Useful commands: pd.DataFrame(), st.dataframe(), st.columns(), with col1:, np.random.randint(), np.sort(), st.select_slider(), st.selected_control(), st.success(), st.error(), st.info(), st.session_state.<var_name>")
 
-    st.write("My trivia description")
+    st.write("Let's see how well you know these timeless dance idols.")
 
     peak_activity_approx = [2005, 1920, 2020, 2010, 1995]
-    dance_style = ["Latin", "Jazz", "Urban", "Experimental", "Jazz"]
+    dance_style = ["Latin", "Physical Theatre", "Urban", "Experimental", "Jazz"]
     
     dancer_df = pd.DataFrame({
         "dancer": favourite_things,
@@ -78,8 +78,8 @@ with tab2:
 
         dancer = st.session_state.dancer
 
-        st.info(dancer)
         year_guess = st.select_slider(question1, np.sort(peak_activity_approx), value=None)
+        st.info(dancer)
 
         submit1 = None
         submit1 = st.button("Submit year")
@@ -101,15 +101,17 @@ with tab2:
             st.session_state.style = dancer_df['style'][style_id]
 
         style = st.session_state.style
-        st.info(style)
+        
 
         dancer_guess = st.segmented_control(question2,dancer_df['dancer'])
-        st.write(dancer_guess)
+        st.info(style)
         answer2 = dancer_df[dancer_df["style"]==style]["dancer"].tolist()
         submit2 = st.button("Submit dancer")
         if submit2:
-            st.write(answer2)
-            st.write(dancer_guess in answer2)
+            if answer2 == dancer_guess:
+                st.success("Wow! You know", dancer_guess, "well!")
+            else:
+                st.error("Not really.",answer2,"is best known as a ", dancer_df[dancer_df["dancer"]==answer2[0]]['style'].iloc[0]," dancer!")
 
 
 
@@ -122,10 +124,10 @@ with tab3:
         st.checkbox("Find at least 2 numeric features that would help determine the answer on the same scale. i.e. book length and original content in the 1-5 out of 5 scale", key="3.2")
         st.checkbox("Complete the dataframe with these new features for the existing objects.", key="3.3")
         st.checkbox("Ask the user to order the preferences from most important to least important features.", key="3.4")
-        st.checkbox("Create 2 models which will give advice at the same time. The first model generates the recommendations using a weighted sum approach of user preference and object score, while the other is fully random.", key="3.5")
-        st.checkbox("Provide both results at the same time, and ask the user which recommendation matches their preference.", key="3.6")
-        st.checkbox("Upon selection, show the rankings and tell the user if they were right about their assumed preferences.", key="3.7")
-        st.info("Useful commands: st.header(), st.write(), st.selectbox(), st.select_slider(), st.button(), st.balloons() (maybe try st radio too)")
+        st.checkbox("Create a model that returs the recommendations using a weighted sum approach of user preference and feature scores.", key="3.5")
+        st.checkbox("Ask the user if they can figure out your own preferred ordering that lead to the answer from the first exercise.", key="3.6")
+        st.checkbox("Add a  conditional pop-up for when the user selection is the same as the author's.")
+        st.info("Useful commands: st.subheader(), st.write(), st.selectbox(), st.button(), st.info()/success()/error(), st.balloons()")
     
 
     st.subheader("Who is your dance idol mentor?")
@@ -167,7 +169,10 @@ with tab3:
     submit_rec = st.button("Find out!", key = "rec_submit")
 
     if or4 and submit_rec:
-        
+
+        if [or1, or2, or3, or4] == ["versatility", "creativity", "personality", "skill"]:
+            st.info("What a coincidence! That's my preference too! We should be dance buddies!")
+            st.balloons()
 
         dancer_df["scores"] = (4 * dancer_df[or1] +
                                3 * dancer_df[or2] +
@@ -184,5 +189,5 @@ with tab3:
         st.dataframe(dancer_df)
 
     if or4 and submit_rec:
-         with st.expander("Dancer scores:"):
+         with st.expander("Dancer scores"):
             st.dataframe(dancer_df[["dancer","scores"]].sort_values(by="scores", ascending=False))
