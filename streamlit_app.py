d56527d6ac60ff3cab810a661c081b5c85bebea0
Cristina Melnic <103945852+cristina-v-melnic@users.noreply.github.com>
2026-09-09 21:24:23 +0200

    Fix indentation and update exercise 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ tab1, tab2, tab3 = st.tabs([":dog: Top dog", "🏆 Ultimate trivia", "🔮 Recom
 with tab1:
     
 
-with st.expander("**Exercise 1** Guess my favourite!"):
+    with st.expander("**Exercise 1** Guess my favourite!"):
         st.write("Please create:")
         st.checkbox("A list of comparable things you like ex. movies, books, songs, artists, foods, ice-creams etc.", key="1.1")
         st.checkbox("A selection interface, i.e. drop-down", key="1.2")
@@ -70,16 +70,16 @@ with tab2:
             
     with c1:
         with st.expander("**Exercise 3** (Un)beatable advice"):
-        st.write("Please create:")
-        st.checkbox("A relevant question to help the user find their next read/practice/inspiration/product/meal. i.e. Which book fits my interests right now?", key="3.1")
-        st.checkbox("Find at least 2 numeric features that would help determine the answer on the same scale. i.e. book length and original content in the 1-5 out of 5 scale", key="3.2")
-        st.checkbox("Complete the dataframe with these new features for the existing objects.", key="3.3")
-        st.checkbox("Ask the user to order the preferences from most important to least important features.", key="3.4")
-        st.checkbox("Create a model that returs the recommendations using a weighted sum approach of user preference and feature scores.", key="3.5")
-        st.checkbox("Ask the user if they can figure out your own preferred ordering that lead to the answer from the first exercise.", key="3.6")
-        st.checkbox("Add a  conditional pop-up for when the user selection is the same as the author's.")
-        st.info("Useful commands: st.subheader(), st.write(), st.selectbox(), st.button(), st.info()/success()/error(), st.balloons()")
-    
+            st.write("Please create:")
+            st.checkbox("A relevant question to help the user find their next read/practice/inspiration/product/meal. i.e. Which book fits my interests right now?", key="3.1")
+            st.checkbox("Find at least 2 numeric features that would help determine the answer on the same scale. i.e. book length and original content in the 1-5 out of 5 scale", key="3.2")
+            st.checkbox("Complete the dataframe with these new features for the existing objects.", key="3.3")
+            st.checkbox("Ask the user to order the preferences from most important to least important features.", key="3.4")
+            st.checkbox("Create a model that returs the recommendations using a weighted sum approach of user preference and feature scores.", key="3.5")
+            st.checkbox("Ask the user if they can figure out your own preferred ordering that lead to the answer from the first exercise.", key="3.6")
+            st.checkbox("Add a  conditional pop-up for when the user selection is the same as the author's.")
+            st.info("Useful commands: st.subheader(), st.write(), st.selectbox(), st.button(), st.info()/success()/error(), st.balloons()")
+        
         question1 = "Around which year did this artist have their peak success?"
         dancers = dancer_df['dancer'].unique()
 
